943670b3de043c9c2f044a2e91027bff234ea276
Paul Derrick Ong <ong_paulderrick@yahoo.com>
2015-12-30 01:19:01 +0800

    Added logging

--- SpringMvcDemo/src/org/maverick/controller/MovieController.java
+++ SpringMvcDemo/src/org/maverick/controller/MovieController.java
@@ -1,6 +1,8 @@
 package org.maverick.controller;
 
 import org.maverick.service.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +14,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MovieController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
+	
 	@Autowired
 	private MovieService movieService;
 
 	@RequestMapping(value = "/movies", method = RequestMethod.GET)
 	public @ResponseBody String findAll(ModelMap model) {
+		logger.info("Finding all movies");
 		return movieService.findAll();
 	}
 	
@@ -24,6 +29,7 @@ public class MovieController {
 	public String Index(Model model) {
 		model.addAttribute("greeting", "Welcome");
 		
+		logger.info("Invoking the greeting function");
 		return "main";
 	}
 }
